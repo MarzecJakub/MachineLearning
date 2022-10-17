52da25ae9240dc9f7278d7223c3841106b68ec9b
MarzecJakub <jakub.marzec@edu.uekat.pl>
2022-10-17 18:30:30 +0200

    zadania 1-3 po zmianach wg pycodestyle

--- zad1.py
+++ zad1.py
@@ -1,25 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 17 13:34:24 2022
-
-@author: Marzec
-"""
-
-#zadanie 1
-
 class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
-        
+
     def is_passed(self):
-        if(sum(self.marks)/len(self.marks) > 50):
+        if (sum(self.marks) / len(self.marks) > 50):
             return True
         else:
             return False
-        
 
-kuba = Student("Jakub", [90,40,50,50,40,50,50,50,50])
+
+kuba = Student("Jakub", [90, 40, 50, 50, 40, 50, 50, 50, 50])
 print(kuba.is_passed())
-michal = Student("Michal", [30,20,20,30,20,70,30,10,10])
-print(michal.is_passed())     
+
+michal = Student("Michal", [30, 20, 20, 30, 20, 70, 30, 10, 10])
+print(michal.is_passed())
