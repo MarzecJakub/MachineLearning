0004029d2e65b886c24fd0f3e2075ce68a1c3deb
MarzecJakub <jakub.marzec@edu.uekat.pl>
2022-10-17 18:45:00 +0200

    zadanie 2 z lab 2 poprawione wg. pycodestyle

--- zad_2.py
+++ zad_2.py
@@ -1,41 +1,45 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 10 13:43:59 2022
-
-@author: Marzec
-"""
-
 #zad2a
 def wyswietl(lista):
     for el in lista:
         print(el)
-    
-lista = ["Ola","Jakub","Agata","Michał","Kacper"]
+
+
+lista = ["Ola", "Jakub", "Agata", "Michał", "Kacper"]
 wyswietl(lista)
 
 
 #zad2b
-liczby = [1,2,3,4,5]
+liczby = [1, 2, 3, 4, 5]
+
+
 def wyswietl(liczby):
     for i in range(len(liczby)):
         liczby[i] = liczby[i] * 2
     return liczby
+
+
 print(wyswietl(liczby))
 
-liczby = [1,2,3,4,5]
+
+liczby = [1, 2, 3, 4, 5]
+
+
 def wyswietlv2(liczby):
     liczby = [el*2 for el in liczby]
     return liczby
+
+
 print(wyswietlv2(liczby))
 
 
 #zad2c
 def wyswietlParzyste(lista):
     for i in range(len(lista)):
-        if lista[i]%2==0:
+        if lista[i] % 2 == 0:
             print(lista[i])
-            
-lista = [2,3,4,6,8,11,10,15,17,12]
+
+
+lista = [2, 3, 4, 6, 8, 11, 10, 15, 17, 12]
 wyswietlParzyste(lista)
 
 
@@ -44,5 +48,6 @@ def wyswietlCoDruga(lista):
     for el in lista[::2]:
         print(el)
 
-lista = [2,3,4,6,8,11,10,15,17,12]
+
+lista = [2, 3, 4, 6, 8, 11, 10, 15, 17, 12]
 wyswietlCoDruga(lista)
