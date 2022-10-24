4ac90924ef7930170b6227405f1df481cd555a6e
MarzecJakub <jakub.marzec@edu.uekat.pl>
2022-10-24 14:04:18 +0200

    zadanie importy

---
+++ magazine/utils.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 13:39:36 2022
+
+@author: Marzec
+"""
+
---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 13:39:36 2022
+
+@author: Marzec
+"""
+
+from magazine import Product
