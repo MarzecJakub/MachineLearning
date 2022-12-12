4903a21228d90daafba740fc98c4357be600ff23
MarzecJakub <jakub.marzec@edu.uekat.pl>
2022-12-12 15:28:31 +0100

    wersja z usunietym tlem

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -9,14 +9,23 @@ upper_red_d = np.array([5,255,255])
 lower_red = np.array([170,150,50])
 upper_red = np.array([180,255,255])
 
-lower_blue = np.array([94,80,2])
-upper_blue = np.array([126,255,255])
+#lower_blue = np.array([94,80,2])
+#upper_blue = np.array([126,255,255])
 
+lower_blue = np.array([100,160,30])
+upper_blue = np.array([120,255,255])
+
+
+kernel_o = np.ones((6,6),np.uint8)
+kernel_c = np.ones((10,10),np.uint8)
 
 cap = cv2.VideoCapture('one_round_cam.mp4')
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 50.0, (1920,1080), True)
+out = cv2.VideoWriter('output_test.mp4', fourcc, 50.0, (1920,1080), True)
+
+
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while(cap.isOpened()):
 
@@ -31,11 +40,14 @@ while(cap.isOpened()):
     mask1 = cv2.inRange(hsv, lower_blue, upper_blue)
     mask_red1 = cv2.inRange(hsv, lower_red, upper_red)
     mask_red2 = cv2.inRange(hsv, lower_red_d, upper_red_d)
-    mask = mask1 + mask_red1 + mask_red2
+    fgmask = fgbg.apply(hsv)
+
+    res = cv2.bitwise_and(frame, frame, mask = ((mask1|mask_red1|mask_red2)&fgmask))
     
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-    out.write(res)
-    cv2.imshow('res',res)
+    cleanedup_frame = cv2.morphologyEx(cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel_o), cv2.MORPH_CLOSE, kernel_c)
+    
+    out.write(cleanedup_frame)
+    cv2.imshow('cleanedup_frame',cleanedup_frame)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
